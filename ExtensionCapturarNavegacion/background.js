845040e25a6f7a446559c828f6ebcdc9e9a2b314
SIASoftwareFactory <SIASoftwareFactory@gmail.com>
2020-09-16 13:51:45 +0200

    Añadir primeros archivos

---
+++ ExtensionCapturarNavegacion/background.js
@@ -0,0 +1,279 @@
+//Icono de activado
+var iconAct = "img/activate.png";
+//Icono desactivado
+var iconDes = "img/desactivate.png";
+//Array donde se guardaran los eventos que vayan grabando
+var secuencia = [];
+// Inicializacion del objeto que se va guardando
+var mensaje = {};
+// Variable para que empiece o termine dependiendo de si es la primera o segunda vez de pulsar el
+//icono de la extension
+var pulsado = true;
+// Variable para motrar mensajes por la consola del navegador
+var debug = true;
+// Variable para comparar los cambios de frame.
+var frameViejo;
+// Variable para guardar el string de los metodos java
+var javaFunciones;
+//Se pone en funcionamiento o se apaga solo cuando se le da click al icono
+chrome.browserAction.onClicked.addListener(function() {
+    if (pulsado) {
+        empezando();
+        pulsado = !pulsado;
+    } else {
+        terminando();
+        pulsado = !pulsado;
+    }
+});
+
+function empezando() {
+    log(">>>>>>>>>> En funcionamiento <<<<<<<<<<");
+    crearNotificacion("on", "Empezando a grabar", iconAct, "Empezando a grabar", 3000);
+    chrome.tabs.query({
+        'active': true,
+        'lastFocusedWindow': true
+    }, function(tabs) {
+        mensaje = {
+            url: tabs[0].url
+        }
+        console.log(mensaje);
+        secuencia.push(mensaje);
+        log("Nº de secuencia >>>>>> " + secuencia.length);
+    });
+    //Recibe los mensaje del content con el objeto del evento
+    chrome.runtime.onMessage.addListener(oyente);
+}
+// Funcion que se llama cuando se pulsa por segunda vez el icono para terminar de grabar
+// y abre la ventana de Guardado y vuelve a inicializar las variables
+function terminando() {
+    chrome.downloads.download({
+        url: saveFunctionJava(),
+        filename: "Test_unitario.txt",
+        saveAs: true
+    });
+    secuencia = [];
+    mensaje = {};
+    frameViejo = "";
+    javaFunciones = "";
+    crearNotificacion("off", "Terminando de grabar", iconDes, "Terminando de grabar", 1500);
+    log(">>>>>>> Terminando <<<<<<");
+    chrome.runtime.onMessage.removeListener(oyente);
+}
+// Es la funcion que se le llama para recibir el mensaje y se vuelve a llamar cuando se termina para 
+// el "removeListener"
+const oyente = function listener(request, sender, sendResponse) {
+
+    mensaje = {
+        id: request.id,
+        name: request.name,
+        elementType: request.elementType,
+        typeEvent: request.typeEvent,
+        value: request.value,
+        linkText: request.linkText,
+        innerText: request.innerText,
+        path: request.path,
+        valueSelect:request.valueSelect,
+        altImg: request.altImg,
+        className:request.className,
+        srcType:request.srcType,
+        frame:request.frame
+    }
+    console.log(mensaje);
+    //Guardamos los objetos segun van llegando
+    secuencia.push(mensaje);
+    log("Nº de secuencia >>>>>> " + secuencia.length);
+}
+// Funcion que crea notificaciones solo cuando empieza la grabacion y cuando termina
+function crearNotificacion(idMsg, titulo, icono, msj, tiempo) {
+    chrome.notifications.create(idMsg, {
+        type: 'basic',
+        title: titulo,
+        iconUrl: icono,
+        message: msj
+    });
+    setTimeout(function() {
+        chrome.notifications.clear(idMsg);
+    }, tiempo);
+    //Cambia el icono a activado
+    chrome.browserAction.setIcon({
+        path: icono
+    });
+}
+// Muestra los mensajes en consola si se pone la variable "debug" a true
+function log(mensaje) {
+    if (debug) {
+        console.log(mensaje);
+    }
+}
+// Funcion que se utiliza para guardar la informacion en un archivo
+// Funcion que se utiliza para guardar la informacion en un archivo
+function save() {
+    var htmlContent = "";
+    for (i in secuencia) {
+        if (i == 0) {
+            htmlContent = ["/** " + secuencia[i].url + "\n"];
+        } else {
+            htmlContent = [htmlContent + 
+            "ID: " + secuencia[i].id + 
+            " - NAME: " + secuencia[i].name + 
+            " - ELEMENT_TYPE: " + secuencia[i].elementType + 
+            " - TYPE_EVENT: " + secuencia[i].typeEvent + 
+            " - VALUE: " + secuencia[i].value + 
+            " - LINKTEXT: " + secuencia[i].linkText + 
+            " - VALUESELECT: " + secuencia[i].valueSelect + 
+            " - ALTIMG: " + secuencia[i].altImg + 
+            " - TARGET: " + secuencia[i].className + 
+            " - SRCTYPE: " + secuencia[i].srcType + 
+            "\nPATH: " + secuencia[i].path.toLowerCase() + "\n"];
+        }
+    }
+    htmlContent = htmlContent + "**/\n";
+    return htmlContent;
+}
+// Funcion que se utiliza para guardar la informacion en un archivo con metodos Java
+function saveFunctionJava() {
+    var htmlContent;
+    htmlContent = [
+    "/**" + "\n"
+        + "* TESTLINK ->"+ "\n"
+        + "*" + "\n"
+        + "*    titulo="+"\n"
+        + "*    descripcion="+"\n"
+        + "*    precondiciones="+"\n"
+        + "*    idRequisitosCubiertos="+"\n"
+    + "**/" + "\n"
+    +"public class Test_unitario {" + "\n"
+    + "@Test" + "\n" 
+    + "public void test" + Date.now() + "() throws Exception {"+ "\n" 
+    + diferenciarEventos(secuencia) + "}"+ "\n" + "}"];  
+    
+    var bl = new Blob(htmlContent, {
+        type: "text/txt"
+    });
+    return URL.createObjectURL(bl);
+}
+// Funcion que escribe los metodos de java de los click.
+function waitElementAndClick(objeto){
+    if (objeto.id != undefined && objeto.id != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.id(" + "\"" + objeto.id + "\"));" + "\n"];
+    } else if (objeto.name != undefined && objeto.name != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.name(" + "\"" + objeto.name + "\"));" + "\n"];
+    } else if (objeto.linkText != undefined && objeto.linkText != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.linkText(" + "\"" + objeto.linkText + "\"));" + "\n"];
+    } else if (objeto.path != undefined && objeto.path != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + objeto.path + "\"));" + "\n"];
+    } else {
+        javaFunciones = [javaFunciones + "\tNo se ha podido identificar el evento" + "\n"];
+    }
+}
+// Funcion que escribe los metodos de java de los change.
+function waitElementAndSendKeys(objeto){
+    if (objeto.id != undefined && objeto.id != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndSendKeys(By.id(" + "\"" + objeto.id + "\")" + ", \"" + objeto.value + "\");" + "\n"];
+    } else if (objeto.name != undefined && objeto.name != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndSendKeys(By.name(" + "\"" + objeto.name + "\")" + ", \"" + objeto.value + "\");" + "\n"];
+    } else if (objeto.path != undefined && objeto.path != "") {
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + objeto.path + "[contains(text(),'" + objeto.linkText + "')]\"));" + "\n"];
+    } else {
+        javaFunciones = [javaFunciones + "\tNo se ha podido identificar el evento" + "\n"];
+    }
+}
+// Funcion que escribe los metodos de java de los select de los change
+function waitElementAndSelect(objeto){
+    if (objeto.id != undefined && objeto.id != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndSelect(By.id(" + "\"" + objeto.id + "\")," + "\"" + objeto.valueSelect + "\");"+ "\n"];
+    }else{
+        javaFunciones = [javaFunciones + "\twaitElementAndSelect(By.id(" + "\"" + objeto.name + "\")," + "\"" + objeto.valueSelect + "\"));"+ "\n"];
+    }
+}
+// Funcion que escribe los metodos de java de Submit y Reset
+function waitElementAndClick_Submit_Reset(objeto){
+    if(objeto.className != undefined && objeto.className != ""){
+        if(objeto.name != undefined && objeto.name != ""){
+            javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.srcType +"' and @class='"+ objeto.className  +"' and @name='"+ objeto.name +"']\"));" + "\n"];
+        }else{
+            javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.srcType +"' and @class='"+ objeto.className +"']\"));" + "\n"];  
+        }    
+    }else if(objeto.name != undefined && objeto.name != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.srcType +"' and @name='"+ objeto.name +"']\"));" + "\n"];
+       
+    }else if(objeto.id != undefined && objeto.id != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.srcType +"' and @id='"+ objeto.id +"']\"));" + "\n"];
+       
+    }else if(objeto.value != undefined && objeto.value != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.srcType +"' and @value='"+ objeto.value +"']\"));" + "\n"];
+    }else{
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + objeto.path + "\"));" + "\n"];
+    }
+}
+
+function waitElementAndClick_button(objeto){
+    if(objeto.className != undefined && objeto.className != ""){
+        if(objeto.id != undefined && objeto.id != ""){
+            javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.typeEvent +"' and @class='"+ objeto.className  +"' and @id='"+ objeto.id +"']\"));" + "\n"];
+        }else{
+            javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.typeEvent +"' and @class='"+ objeto.className +"']\"));" + "\n"];  
+        }    
+    }else if(objeto.id != undefined && objeto.id != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.typeEvent +"' and @id='"+ objeto.id +"']\"));" + "\n"];  
+    }else if(objeto.value != undefined && objeto.value != ""){
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(\"//input[@type='"+ objeto.typeEvent +"' and @value='"+ objeto.value +"']\"));" + "\n"];
+    }else{
+        javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + objeto.path + "\"));" + "\n"];
+    }
+}
+function diferenciarEventos(secuencia) {
+    javaFunciones= "";
+    if (secuencia[0].url != "") {
+        javaFunciones = [javaFunciones + "\tdriver.get(" + "\"" + secuencia[0].url + "\"" + ");" + "\n"];
+    }
+    for (i in secuencia) {
+        // Diferenciamos los eventos si es un click
+        if (secuencia[i].typeEvent == "click") {
+            // Capturamos en un método cuando hay un cambio de frame
+           if(secuencia[i].frame != frameViejo && secuencia[i].frame != ""){
+                frameViejo = secuencia[i].frame;
+                javaFunciones = [javaFunciones +"\tchangeParentFrame();\n" +"\tchangeFrame(" + "\"" + secuencia[i].frame + "\");" + "\n"];
+            }else{
+                frameViejo = "";
+            }
+            if (secuencia[i].elementType == "a") {
+                javaFunciones = [javaFunciones + "\twaitElementAndClick(By.linkText(" + "\"" + secuencia[i].linkText + "\"));" + "\n"];
+                // Con este if se puede extraer todos los td que puedan estar dentro de un tr
+                // } else if (secuencia[i].elementType == "td") {
+                //     javaFunciones = [javaFunciones + "waitElementAndClick(By.xpath(" + "\"" + secuencia[i].path + "[" + secuencia[i].innerText + "]\"));" + "\n"];
+            } else if (secuencia[i].elementType == "img") {
+                if(secuencia[i].id != "" && secuencia[i].id != undefined){
+                    javaFunciones = [javaFunciones + "\twaitElementAndClick(By.id(" + "\"" + secuencia[i].id + "\"));" + "\n"];
+                }else{
+                    javaFunciones = [javaFunciones + "\twaitElementAndClick(By.cssSelector(\"img[alt=\\"+"\"" + secuencia[i].altImg + "\\\"]\"), By.xpath(" + "\"" + secuencia[i].path + "\"));" + "\n"];
+                }
+            } else if (secuencia[i].elementType == "td") {
+                javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + secuencia[i].path + "[contains(text(),'" + secuencia[i].linkText + "')]\"));" + "\n"];
+            } else if (secuencia[i].elementType == "span") {
+                javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + secuencia[i].path + "[contains(text(),'" + secuencia[i].linkText + "')]\"));" + "\n"];
+            } else if (secuencia[i].srcType == "submit") {
+                if (secuencia[i].elementType == "button"){
+                    javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + secuencia[i].path + "[contains(text(),'" + secuencia[i].linkText + "')]\"));" + "\n"];
+                }else{
+                waitElementAndClick_Submit_Reset(secuencia[i]);
+                }
+            } else if (secuencia[i].srcType == "reset") {
+                waitElementAndClick_Submit_Reset(secuencia[i]);
+            } else {
+               waitElementAndClick(secuencia[i]);
+            }
+        } else if (secuencia[i].typeEvent == "change") {
+            if (secuencia[i].elementType == "select") {
+               waitElementAndSelect(secuencia[i]);            
+            }else {
+               waitElementAndSendKeys(secuencia[i]);
+            }
+        }else if (secuencia[i].typeEvent == "checkbox") {
+            waitElementAndClick(secuencia[i]);
+        }else if (secuencia[i].typeEvent == "radio") {
+            javaFunciones = [javaFunciones + "\twaitElementAndClick(By.xpath(" + "\"" + secuencia[i].path + "\"));" + "\n"];
+        }
+    }
+    return javaFunciones;
+}
---
+++ ExtensionCapturarNavegacion/content.js
@@ -0,0 +1,136 @@
+// Constructor del objeto que luego queremos enviar al background
+
+function DatosEvento(id, name, elementType, typeEvent, value, linkText, innerText, path, valueSelect, altImg, className,srcType,frame) {
+    this.id = id;
+    this.name = name;
+    this.elementType = elementType;
+    this.typeEvent = typeEvent;
+    this.value = value;
+    this.linkText = linkText;
+    this.innerText = innerText;
+    this.path = path;
+    this.valueSelect = valueSelect;
+    this.altImg = altImg;
+    this.className = className;
+    this.srcType = srcType;
+    this.frame = frame;
+}
+
+document.addEventListener("click", clickHandler);
+document.addEventListener("change", updateValue);
+
+//Es la funcion que se encarga de recoger el evento del click
+function clickHandler(event) {
+    //Inicializa la variable "mandar" a falso para que solo cuando es un click valido se mande la informacion
+    // al background
+   
+    console.log(event);
+    var mandar = false;
+    var elementType = event.srcElement.localName;
+    var path = "";
+    var altImagen = "";
+    var className="";
+    var srcType="";
+    
+
+   // Para poder separar los input de submit y reset y se comporten como un click.
+     if (event.srcElement.type == "submit" || event.srcElement.type == "reset"){
+            srcType = event.srcElement.type;
+            className = event.srcElement.className;
+        };
+    //Se carga con la informacion dependiendo de donde haya dado click, y solo en los elementos que queremos
+    if (elementType == 'span' ||elementType == 'img' || elementType == 'td' || elementType == 'a' || elementType == 'button' || srcType =="submit" || srcType =="reset" || srcType =="type") {
+        // con esto recogemos todo el path del evento y lo guardamos en un string(lo ponemos a lenght -2 para que tome desde html)
+        for (var i = 0; i < event.path.length - 2; i++) {
+            path = "/" + event.path[i].nodeName + path;
+        };
+        // Con este for recorremos el tr para guardar los td que puedan estar dentro.
+        // for (var i = 0; i < event.path[1].cells.length; i++) {
+        //    var pathCells = pathCells + "<td>" + event.path[1].cells[i].innerText + "</td>";
+        // }
+        // Captamos alt para información de la imagen pulsada
+        if (event.srcElement.localName == "img") {
+            altImagen = event.srcElement.alt;
+        };
+       
+        // Volcamos todos los datos a nuestro objeto para enviarlo
+        var miobjeto = new DatosEvento (
+            event.srcElement.id, 
+            event.srcElement.name, 
+            elementType, 
+            event.type, 
+            event.srcElement.value, 
+            event.srcElement.textContent, 
+            null,
+            path.toLowerCase(), 
+            null,
+            altImagen,
+            className,
+            srcType,
+            event.view.name
+            );
+        mandar = true;
+    };
+    //Solo si la varible "mandar" es igual a true, es decir a dado en un elemento valido
+    // se envia la informacion
+    if (mandar) {
+       if(typeof chrome.app.isInstalled!=='undefined'){
+        chrome.runtime.sendMessage(miobjeto);
+        };
+    };
+};
+
+function updateValue(e) {
+    console.log(e);
+    var path = "";
+    var valueSelect = "";
+    var type = e.type;
+    var srcType = null;
+    var className="";
+    var frameActual;
+    
+    for (var i = 0; i < e.path.length - 2; i++) {
+        path = "/" + event.path[i].nodeName + path;
+    };
+    //Para recoger el frame en que se ha producido el evento si es que tiene
+    for (var i = 0; i < e.path.length; i++) {
+        if(e.path[i].name !== undefined){
+             frameActual = e.path[i].name;
+        }
+    };
+  
+    if (e.srcElement.localName == "select") {
+        valueSelect = e.srcElement.selectedOptions[0].innerText;
+    };
+    // Para cambiarle la propiedad a type si es que marcan un text y deje de ser input
+    if (e.srcElement.type == "text"){
+        srcType = e.srcElement.type;   
+    };
+
+    // Para cambiarle la propiedad a type si es que marcan un checkbox o un radio y deje de ser input
+    if (e.srcElement.type == "checkbox") {
+        type = e.srcElement.type;
+        className = e.srcElement.className;
+    };
+    if (e.srcElement.type == "radio") {
+        type = e.srcElement.type;
+    };
+
+    var datos = new DatosEvento(
+        e.srcElement.id, 
+        e.srcElement.name, 
+        e.srcElement.localName, 
+        type, 
+        e.srcElement.value, 
+        e.srcElement.textContent, 
+        null, 
+        path.toLowerCase(), 
+        valueSelect,
+        null,
+        className,
+        srcType,
+        frameActual
+        );
+
+    chrome.runtime.sendMessage(datos);
+};
